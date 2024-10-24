00f10c29566479c9e057040a94328342f3f2ce2c
hendisantika <hendisantika@yahoo.co.id>
2024-10-25 06:48:34 +0700

    Create Spring Boot App🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/oauth2jtedemo/SpringBootOauth2JteDemoApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.oauth2jtedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOauth2JteDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootOauth2JteDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/oauth2jtedemo/SpringBootOauth2JteDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.oauth2jtedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOauth2JteDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
